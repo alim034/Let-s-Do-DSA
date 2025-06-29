480159da803b8f791c0d3adab0febdb80050d28d
Mohammad Alim <alimmohammad034@gmail.com>
2025-06-30 00:45:39 +0530

    Leetcode Problems based on Fibonacci Series

---
+++ src/LeetCode_Problems/Leetcode125.java
@@ -0,0 +1,46 @@
+package LeetCode_Problems;
+
+public class Leetcode125 {
+    public static void main(String[] args) {
+        String str = "A man, a plan, a canal: Panama";
+
+        System.out.println(isPalindrom(str));
+    }
+
+    public static boolean isPalindrom(String s) {
+        String str = s.toLowerCase();
+        String str1 = clean(str);
+
+        // String Reverse Logic
+        String reverse = "";
+        char[] ch = str1.toCharArray();
+        for(int i=str1.length()-1; i>=0; i--) {
+            reverse = reverse + ch[i];
+        }
+
+        // checking for palindrom
+        if(str1.equals(reverse)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static String clean(String s) {
+        String str = s;
+        String str1 = "";
+
+        for(int i=0; i<str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if(ch>='a' && ch<='z' ||
+                ch>='A' && ch<='Z' ||
+                ch>='0' && ch<='9') {
+                str1 = str1+ch;
+            } else {
+
+            }
+        }
+        return str1;
+    }
+}
