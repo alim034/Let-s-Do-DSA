d0a5b001779565da7b417f0cef24e14fc30e73b2
Mohammad Alim <alimmohammad034@gmail.com>
2025-06-30 01:31:40 +0530

    Code to Check Array is Sorted or Not

---
+++ src/Arrays/CheckIsSorted.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class CheckIsSorted {
+    public static void main(String[] args) {
+//        int[] arr = {1,2,3,1,5,6,7,8,8,9};
+//        int[] arr = {1,2,3,4,5,5,6,6,7};
+        int[] arr = {-1,-2,10};
+
+        System.out.println(isSorted(arr));
+    }
+
+    public static boolean isSorted(int[] arr) {
+        int len = arr.length;
+
+        for (int i=0; i<len-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
