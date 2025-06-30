147628ce22ed0e502909f3d82a651833635b2625
Mohammad Alim <alimmohammad034@gmail.com>
2025-07-01 02:26:12 +0530

    Remove Duplicate elements from array code

---
+++ src/LeetCode_Problems/Leetcode26.java
@@ -0,0 +1,24 @@
+package LeetCode_Problems;
+
+public class Leetcode26 {
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,2,2,3,3,3,4,4};
+
+        int j = removeDuplicate(arr);
+        for(int i=0; i<j; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static int removeDuplicate(int[] arr) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+    }
+}
