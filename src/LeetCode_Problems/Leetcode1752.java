fc570fdf5619667acfc622e2ff72012c08cc5a1c
Mohammad Alim <alimmohammad034@gmail.com>
2025-07-01 00:34:05 +0530

    check if array is sorted and rotated

---
+++ src/LeetCode_Problems/Leetcode1752.java
@@ -0,0 +1,24 @@
+package LeetCode_Problems;
+
+public class Leetcode1752 {
+    public static void main(String[] args) {
+//        int[] arr = {3,4,5,1,2};
+        int[] arr = {2,1,3,4};
+
+        System.out.println(check(arr));
+    }
+
+    public static boolean check(int[] arr) {
+        int n = arr.length;
+
+        int cnt = 0;
+
+        for(int i=0; i<n; i++) {
+            if(arr[i] > arr[(i+1) % n]) {
+                cnt++;
+            }
+        }
+
+        return cnt <= 1;
+    }
+}
