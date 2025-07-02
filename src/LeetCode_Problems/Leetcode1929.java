0f84b3d77e4861b1fb193d43538510700b3bcb3f
Mohammad Alim <alimmohammad034@gmail.com>
2025-07-03 00:43:29 +0530

    Leetcode problem for Concatenation of two arrays

---
+++ src/LeetCode_Problems/Leetcode1929.java
@@ -0,0 +1,30 @@
+package LeetCode_Problems;
+
+import java.util.ArrayList;
+
+public class Leetcode1929 {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+
+        int[] arr1 = getConcatenation(arr);
+
+        for(int i: arr1) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+
+        int[] ans = new int[n*2];
+//        System.out.println(ans.length);
+
+       for(int i=0; i< n; i++) {
+           ans[i] = nums[i];
+           ans[i+n] = nums[i];
+       }
+
+       return ans;
+    }
+}
