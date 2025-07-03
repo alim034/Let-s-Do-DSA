ab14bf0ccf21d6a76443978105ec904bc1cca2bc
Mohammad Alim <alimmohammad034@gmail.com>
2025-07-04 01:34:03 +0530

    Leetcode problem for Shuffle the Array Elements

---
+++ src/LeetCode_Problems/Leetcode1470.java
@@ -0,0 +1,35 @@
+package LeetCode_Problems;
+
+public class Leetcode1470 {
+
+    public static void main(String[] args) {
+
+//        int[] arr = {2,5,1,3,4,7};
+        int[] arr = {1,2,3,4,4,3,2,1};
+        int n = 4;
+
+        int[] ans = shuffleArray(arr , n);
+
+        for(int i: ans) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int[] shuffleArray(int[] nums, int n) {
+
+//        step1: Create new empty array
+
+        int[] ans = new int[2*n];
+
+        // Step2: Create a count variables
+        int count = 0;
+
+        for(int i=0; i<n; i++) {
+            ans[count] = nums[i];
+            ans[count+1] = nums[i+n];
+            count += 2;
+        }
+
+        return ans;
+    }
+}
