ddedcb04c1dc0f5cec5c819fceca62c9e2f771f3
Mohammad Alim <alimmohammad034@gmail.com>
2025-07-05 20:15:03 +0530

    Leetcode problem of array to find numbers of goods pairs

--- src/LeetCode_Problems/Leetcode1512.java
+++ src/LeetCode_Problems/Leetcode1512.java
@@ -1,28 +1,26 @@
 package LeetCode_Problems;
 
 public class Leetcode1512 {
-    public static void main(String[] args) {
-
-//        int[] arr = {1,2,3,1,1,3};
-//        int[] arr = {1,1,1,1};
-        int[] arr = {1,2,3};
-
-        System.out.println(numIdenticalPairs(arr));
-    }
-
     public static int numIdenticalPairs(int[] nums) {
 
         int n = nums.length;
 
         int count = 0;
+
         for(int i=0; i<n; i++) {
             for(int j=1; j<n; j++) {
-                if(nums[i]==nums[j] && i<j) {
+                if(nums[i]==nums[j] && i<j){
                     count++;
                 }
             }
         }
 
         return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,1,1,1};
+
+        System.out.println(numIdenticalPairs(arr));
     }
 }
