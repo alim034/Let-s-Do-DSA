fbe9c7b045b6a12ffcf8cd768f9d8bbbc4314b9f
Mohammad Alim <alimmohammad034@gmail.com>
2025-07-06 19:07:55 +0530

    Leetcode Math Problem to check Number is Armstrong or not.

---
+++ src/Math/Leetcode1134.java
@@ -0,0 +1,34 @@
+package Math;
+
+
+public class Leetcode1134 {
+    public static void main(String[] args) {
+
+        int num = 153;
+
+        System.out.println(isArmstrong(num));
+    }
+
+    public static boolean isArmstrong(int num) {
+
+        if(check(num)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean check(int num) {
+        int originalNumber = num;
+        double sum = 0;
+        int numbersOfDigits = String.valueOf(num).length();
+
+        while(num>0) {
+            int digit = num%10;
+            sum = sum + Math.pow(digit, numbersOfDigits);
+            num = num/10;
+        }
+
+        return originalNumber==sum;
+    }
+}
