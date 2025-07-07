a29ae526363d497e6218e16dd41933849a9934ab
Mohammad Alim <alimmohammad034@gmail.com>
2025-07-08 01:28:01 +0530

    Leetcode Problem Find Missing Number in Array

---
+++ src/LeetCode_Problems/Leetcode268.java
@@ -0,0 +1,47 @@
+package LeetCode_Problems;
+
+import java.util.Arrays;
+
+public class Leetcode268 {
+
+    public static void main(String[] args) {
+
+        int[] nums = {3,0,1};
+//        int[] ans = missingNumber(nums);
+//
+//        for(int i: ans) {
+//            System.out.print(i + " ");
+//        }
+
+        System.out.println(missingNumber(nums));
+    }
+
+    public static int missingNumber(int[] nums) {
+
+        int n = nums.length;
+
+//        Arrays.sort(nums);
+
+//        int missNum = 0;
+//        for(int i=0; i<n; i++) {
+//           for(int j=0; j<n; j++) {
+//               if(nums[i] != nums[j]) {
+//                   missNum = nums[i];
+//               }
+//           }
+//        }
+
+        int arrayElementsSum = 0;
+        for(int i=0; i<n; i++) {
+            arrayElementsSum = arrayElementsSum+nums[i];
+        }
+
+        int actaulSum = (n*(n+1))/2;
+
+        int missNum = actaulSum-arrayElementsSum;
+
+
+
+        return missNum;
+    }
+}
