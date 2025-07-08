602fb3c370eff5088fd83ea211e606203e8e15cb
Mohammad Alim <alimmohammad034@gmail.com>
2025-07-09 01:14:09 +0530

    Leetcode Problem Two Sum

---
+++ src/LeetCode_Problems/Leetcode01.java
@@ -0,0 +1,32 @@
+package LeetCode_Problems;
+
+public class Leetcode01 {
+    public static void main(String[] args) {
+
+        int[] nums = {2,7,11,15};
+        int target = 9;
+
+        int[] ans = twoSum(nums, target);
+
+        for(int i: ans) {
+            System.out.print(i+ " ");
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+
+        int[] indices = new int[2];
+
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                if(nums[i]+nums[j] == target) {
+                    indices[0] = i;
+                    indices[1] = j;
+                }
+            }
+        }
+
+        return indices;
+    }
+}
