a3d38ed53ee1105e8cb9f790abd8d7ac7895cc8f
Mohammad Alim <alimmohammad034@gmail.com>
2025-07-13 12:45:55 +0530

    Target Sum Problem , Calculate the Num of Triplets whos sum equals to the target

---
+++ src/Arrays/TargetSum.java
@@ -0,0 +1,31 @@
+// Find the Number
+package Arrays;
+
+public class TargetSum {
+    public static void main(String[] args) {
+
+        int[] nums = {1,4,5,6,3};
+        int target = 12;
+
+        System.out.println(triplets(nums,target));
+    }
+
+    public static int triplets(int[] nums, int target) {
+
+        int n = nums.length;
+
+        int count = 0;
+
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                for(int k=j+1; k<n; k++) {
+                    if(nums[i]+nums[j]+nums[k] == target) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
