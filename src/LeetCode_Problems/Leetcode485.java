b7dde2008f01b59a68689d89843d8ddec2c2ae57
Mohammad Alim <alimmohammad034@gmail.com>
2025-07-21 02:31:35 +0530

    Leetcode Problem to return the maximum number of consecutive 1's in the array.

---
+++ src/LeetCode_Problems/Leetcode485.java
@@ -0,0 +1,30 @@
+package LeetCode_Problems;
+
+public class Leetcode485 {
+    public static void main(String[] args) {
+        int[] nums = {1,1,0,1,1,1};
+
+        System.out.println(findMaxConsecutiveOnes((nums)));
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+
+        int n = nums.length;
+
+        int count = 0;
+        int maxCount = 0;
+
+        for(int i=0; i<n; i++) {
+            if(nums[i]==1) {
+                count++;
+            } else {
+                count = 0;
+            }
+
+            // Comparing the both counter variables.
+            maxCount = Math.max(count, maxCount);
+        }
+
+        return maxCount;
+    }
+}
