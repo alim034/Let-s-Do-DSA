9dfd5b59dbe290df913a5e109dbdd76b869c04cc
Mohammad Alim <alimmohammad034@gmail.com>
2025-07-22 23:59:47 +0530

    code for find the frequency of element in array.

--- src/Arrays/FrequencyOfElements.java
+++ src/Arrays/FrequencyOfElements.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class FrequencyOfElements {
     public static void main(String[] args) {
 
@@ -7,33 +9,52 @@ public class FrequencyOfElements {
 
         int[] freq = frequecyElement(arr);
 
-        int visited = -1;
-        for(int i = 0; i < freq.length; i++) {
-            if(freq[i] != visited) {
-                System.out.println(arr[i] + " occurs " + freq[i] + " times");
-            }
+//        int visited = -1;
+//        for(int i = 0; i < freq.length; i++) {
+//            if(freq[i] != visited) {
+//                System.out.println(arr[i] + " occurs " + freq[i] + " times");
+//            }
+//        }
+
+//        System.out.println(Arrays.toString(freq)); // it give the whole array.
+
+        for(int i: freq) {
+            System.out.print(i + " ");
         }
     }
 
     public static int[] frequecyElement(int[] nums) {
 
         int n = nums.length;
-        int[] freq = new int[n];
-        int visited = -1;
 
-        for(int i = 0; i < n; i++) {
-            if(freq[i] == visited) {
-                continue; // skip already counted numbers
-            }
+        /* Brute Force for these problem. */
+//        int[] freq = new int[n];
+//        int visited = -1;
+//
+//        for(int i = 0; i < n; i++) {
+//            if(freq[i] == visited) {
+//                continue; // skip already counted numbers
+//            }
+//
+//            int count = 1;
+//            for(int j = i + 1; j < n; j++) {
+//                if(nums[i] == nums[j]) {
+//                    count++;
+//                    freq[j] = visited;
+//                }
+//            }
+//            freq[i] = count;
+//        }
+//
+//        return freq;
 
-            int count = 1;
-            for(int j = i + 1; j < n; j++) {
-                if(nums[i] == nums[j]) {
-                    count++;
-                    freq[j] = visited;
-                }
-            }
-            freq[i] = count;
+        /* Optimal Solution for these problem. */
+
+
+        int[] freq = new int[101];
+
+        for(int num: nums) {
+            freq[num]++;
         }
 
         return freq;
