4e96b4f38076725aa5711c6fd4468abc3bfba0b7
Mohammad Alim <alimmohammad034@gmail.com>
2025-07-30 09:43:59 +0530

    Leetcode Problem: Distribute Candies

---
+++ src/LeetCode_Problems/Leetcode575.java
@@ -0,0 +1,49 @@
+package LeetCode_Problems;
+
+import java.util.Arrays;
+
+public class Leetcode575 {
+
+    public static void main(String[] args) {
+//        int[] nums = {1,1,2,2,3,3};
+//        int[] nums = {1,1,2,3};
+        int[] nums = {6,6,6,6,6};
+
+        System.out.println(distributeCandies(nums));
+    }
+
+    public static int distributeCandies(int[] nums) {
+
+        int n = nums.length;
+
+        int lengthCount = n/2;
+
+        int distinctCount = numOfDistinct(nums);
+
+        if(distinctCount < lengthCount) {
+            return distinctCount;
+        }
+
+        return lengthCount;
+    }
+
+    public static int numOfDistinct(int[] nums) {
+
+        int n = nums.length;
+
+        Arrays.sort(nums);
+        int count = 1;
+
+        if(n == 0) {
+            return 0;
+        }
+
+        for(int i=1; i<n; i++) {
+            if(nums[i] != nums[i-1]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
