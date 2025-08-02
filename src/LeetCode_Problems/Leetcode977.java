4fc014f62e0b6edbb615e743cd038b66dcce1c87
Mohammad Alim <alimmohammad034@gmail.com>
2025-08-03 01:42:31 +0530

    Leetcode Problem: Squares of a Sorted Array

---
+++ src/LeetCode_Problems/Leetcode977.java
@@ -0,0 +1,29 @@
+package LeetCode_Problems;
+import java.util.Arrays;
+
+public class Leetcode977 {
+    public static void main(String[] args) {
+
+//        int[] nums = {-4,-1,0,3,10};
+        int[] nums = {-7,-3,2,3,11};
+
+       int[] ans = sortedSquares(nums);
+
+       for(int i: ans) {
+           System.out.print(i + " ");
+       }
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+
+        int n = nums.length;
+
+        for(int i=0; i<n; i++) {
+            nums[i] = nums[i]*nums[i];
+        }
+
+        Arrays.sort(nums);
+
+        return nums;
+    }
+}
