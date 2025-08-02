d9ae9b358861d4af08bc92b2e6e59407fb5d8976
Mohammad Alim <alimmohammad034@gmail.com>
2025-08-03 00:17:45 +0530

    Leetcode Problem: Sort Colors

---
+++ src/LeetCode_Problems/Leetcode75.java
@@ -0,0 +1,45 @@
+package LeetCode_Problems;
+
+import java.util.Arrays;
+
+public class Leetcode75 {
+    public static void main(String[] args) {
+
+        int[] nums = {2,0,2,1,1,0};
+
+        System.out.println("Array Before Sorting: ");
+        System.out.println(Arrays.toString(nums));
+
+        sortColors(nums);
+
+        System.out.println("Array After Sorting: ");
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void sortColors(int[] nums) {
+
+        int n = nums.length;
+
+        int zeros = 0;
+        int ones = 0;
+
+        for(int i=0; i<n; i++) {
+            if(nums[i] == 0) {
+                zeros++;
+            }
+            if(nums[i] == 1) {
+                ones++;
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            if(i<zeros) {
+                nums[i] = 0;
+            } else if(i < zeros+ones) {
+                nums[i] = 1;
+            } else {
+                nums[i] = 2;
+            }
+        }
+    }
+}
