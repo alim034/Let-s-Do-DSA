d3c52a8a929305681458e6f62b7f4f4c8409a369
Mohammad Alim <alimmohammad034@gmail.com>
2025-08-12 00:53:14 +0530

    Linear Search In range. algo code.

--- src/Arrays_Searching/LinearSearch.java
+++ src/Arrays_Searching/LinearSearch.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class LinearSearch {
 
+    // Function for the Linear Search Algo.
     public static int linearSearch(int[] nums, int target) {
 
         int n = nums.length;
@@ -14,6 +15,24 @@ public class LinearSearch {
 
         for(int i=0; i<n; i++) {
             if(nums[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+
+    // Function for the Linear Search In Range
+    public static int searchInRange(int[] nums,int target, int startRange, int endRange) {
+        int n = nums.length;
+
+        if (n==0) {
+            return -1;
+        }
+
+        for (int i=startRange; i<endRange; i++) {
+            if (nums[i]==target) {
                 return i;
             }
         }
@@ -33,6 +52,20 @@ public class LinearSearch {
             System.out.println("Element " + target +" is Not Found!!!!!");
         } else {
             System.out.println("Element " + target + " is Found at Index: " + ans);
+        }
+
+        // Search in Range
+        int[] nums1 = {1,23,45,75,-3,-55,5,0,5,3,2,88,-90};
+        int target1 = -3;
+        int startRange = 1;
+        int endRange = 5;
+
+        int ans1 = searchInRange(nums1,target1,startRange,endRange);
+
+        if (ans1==-1) {
+            System.out.println("Element " + target +" is Not Found!!!!!");
+        } else {
+            System.out.println("Element " + target + " is Found at Index: " + ans1);
         }
     }
 }
