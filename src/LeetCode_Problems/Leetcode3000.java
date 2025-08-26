18651fb518d3faa79ce33c4978ca443f7788b8aa
Mohammad Alim <alimmohammad034@gmail.com>
2025-08-27 00:56:09 +0530

    Leetcode Problem: Maximum Area of Longest Diagonal Rectangle

---
+++ src/LeetCode_Problems/Leetcode3000.java
@@ -0,0 +1,33 @@
+package LeetCode_Problems;
+
+public class Leetcode3000 {
+    public static void main(String[] args) {
+        int[][] dimensions = {{9,3}, {8,6}};
+
+        System.out.println(areaOfMaxDiagonal(dimensions));
+    }
+
+    public static int areaOfMaxDiagonal(int[][] dimensions) {
+
+        double maxDiagonal = 0;
+        int maxArea = 0;
+
+        for(int i=0; i<dimensions.length; i++) {
+
+            int l = dimensions[i][0];
+            int w = dimensions[i][1];
+
+            double diagonal = l*l + w*w;
+            int area = l*w;
+
+            if(diagonal>maxDiagonal) {
+                maxDiagonal = diagonal;
+                maxArea = area;
+            } else if(diagonal==maxDiagonal) {
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+
+        return maxArea;
+    }
+}
