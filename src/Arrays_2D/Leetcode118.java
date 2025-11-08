600d99478402f1589a6d3d819be48707c2804af1
Mohammad Alim <alimmohammad034@gmail.com>
2025-11-09 02:29:57 +0530

    Leetcode Problem: Pascal Triangle.

--- src/Arrays_2D/Leetcode118.java
+++ src/Arrays_2D/Leetcode118.java
@@ -8,7 +8,7 @@ public class Leetcode118 {
 
     // Method to Print the Matrix
     static void printMatrix(int[][] nums) {
-        for(int i=0; i<nums.length; i++) {
+        for(int i=0; i<nums.length; i++) { // row
             for(int j=0; j<nums[i].length; j++) {
                 System.out.print(nums[i][j] + " ");
             }
